0f4e3f3f800df93dd219a97698dabba79d10b978
micahjp <micahpope6@gmail.com>
2024-08-13 12:38:27 -0500

    added testing for textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,8 +5,21 @@ from textnode import TextNode
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a test node", "bold")
-        node2 = TextNode("This is a test node", "bold")
-        self.assertEqual(node, node2)
+        node1 = TextNode("This is a test node", "bold")
+        self.assertEqual(node, node1)
+
+    def test_not_eq(self):
+        node = TextNode("This is a test node", "bold")
+        node1 = TextNode("This is a test node", "italic")
+        self.assertNotEqual(node, node1)
+
+    def test_repr(self):
+        node = TextNode("This is a test node", "italic", "https://test.com")
+        self.assertEqual(node.__repr__(), f"TextNode({node.text}, {node.text_type}, {node.url})")
+
+    def test_default_url(self):
+        node = TextNode("This is a test node", "underline")
+        self.assertEqual(node.url, None)
 
 
 if __name__ == "__main__":
